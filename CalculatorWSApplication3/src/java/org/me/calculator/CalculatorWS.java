87c7e5b4ff7d5091ab75e543b1d93436f795a2af
Eduardo Diaz <eadiaz96@gmail.com>
2018-03-03 21:16:30 -0500

    quinta

--- CalculatorWSApplication3/src/java/org/me/calculator/CalculatorWS.java
+++ CalculatorWSApplication3/src/java/org/me/calculator/CalculatorWS.java
@@ -20,14 +20,18 @@ public class CalculatorWS {
 
     /**
      * This is a sample web service operation
+     * @param txt
+     * @return 
      */
     @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
+    public String hello(@WebParam(name = "i") String txt) {
         return "Hello " + txt + " !";
     }
 
     /**
      * Web service operation
+     * @param name
+     * @return 
      */
     @WebMethod(operationName = "operation")
     public String operation(@WebParam(name = "name") String name) {
@@ -37,10 +41,13 @@ public class CalculatorWS {
 
     /**
      * Web service operation
+     * @param i
+     * @param j
+     * @return 
      */
     @WebMethod(operationName = "add")
     public int add(@WebParam(name = "i") int i, @WebParam(name = "j") int j) {
-        //TODO write your implementation code here:
-        return 0;
-    }
+        int k = i + j;
+        return k;
+      }
 }
